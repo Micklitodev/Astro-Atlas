3557fc2ad5960ca00a04dad42aa6ea629e052043
Mick L <90425599+Micklitodev@users.noreply.github.com>
2023-05-23 11:52:34 -0400

    0.4.0
    deleted unneeded, server/routes/  and server/conterollers/,  updated server/graphql/resolvers to have resolver methods for save and delete books.

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -4,36 +4,62 @@ const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    currUser: async (_, args, context) => { 
-      if ( context.user ) { 
-      const userData = await User.findOne({ _id: context.user._id }).select(
-        "-_v -password"
-      );
-      return userData;
+    currUser: async (_, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-_v -password"
+        );
+        return userData;
       } else {
-        throw new AuthenticationError('please log in')
+        throw new AuthenticationError("please log in");
       }
     },
   },
 
   Mutation: {
-    createUser: async (_, args ) => { 
-      const user = await User.create(args)
-      const token = signToken(user)
-      return { user, token};
+    createUser: async (_, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { user, token };
     },
-    login: async (_, {email, password}) => {
-      const user = await User.findOne({email})
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
       if (!user) {
-        throw new AuthenticationError('email or password incorrect, please try again. ')
+        throw new AuthenticationError(
+          "email or password incorrect, please try again. "
+        );
       }
-      const pw = await user.isCorrectPassword(password)
+      const pw = await user.isCorrectPassword(password);
       if (!pw) {
-        throw new AuthenticationError('email or password incorrect, please try again. ')
+        throw new AuthenticationError(
+          "email or password incorrect, please try again. "
+        );
       }
-      const token = signToken(user)
-      return { user, token }
-    }
+      const token = signToken(user);
+      return { user, token };
+    },
+    saveBook: async (_, { inputBook }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError("Please log in first!");
+      }
+      const addBookToUser = await User.findByIdAndUpdate(
+        { _id: context.user._id },
+        { $push: { savedBooks: inputBook } },
+        { new: true }
+      );
+      return addBookToUser;
+    },
+    delBook: async (_, { bookId }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError("Please log in first!");
+      }
+      const removeBookFromUser = await User.findByIdAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId } } },
+        { new: true }
+      );
+      return removeBookFromUser;
+    },
   },
 };
 
