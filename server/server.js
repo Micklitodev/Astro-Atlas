0381a55c415b20adeb441fccbb8409748c964b9a
Mick L <90425599+Micklitodev@users.noreply.github.com>
2023-05-25 02:24:44 -0400

    0.9.0
    reverted back to apollo-server-express after i realized that the page would not be served and apollo-server had no way to serve pages without express. updated package.json and server/server.js to use apollo-server-express and to start the express server with the apollo server.

--- server/server.js
+++ server/server.js
@@ -1,27 +1,36 @@
-const { ApolloServer } = require('apollo-server')
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
 
-const db = require('./config/connection')
-const {typeDefs, resolvers} = require('./graphql')
-const { authMiddleware } = require('./utils/auth')
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+const { typeDefs, resolvers } = require("./graphql");
 
-const path = require('path')
-
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-   context: authMiddleware
-})
-
-server.static(path.join(__dirname, '../client/build'));
-
-server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  context: authMiddleware,
 });
 
+server.start().then(() => {
+  server.applyMiddleware({ app });
+});
 
-db.once('open', () => {
-  console.log('success')
-  return server.listen({port: PORT})
-}) 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
